f044e657a4f7337e3969aba9f394a9c5c3497e96
yuvrajkumar7837-jpg <yuvrajkumar7837@gmail.com>
2026-01-12 21:45:59 +0530

    changed

--- accounts/models.py
+++ accounts/models.py
@@ -9,8 +9,6 @@ class user(AbstractUser):
         ('teacher' , 'Teacher')
     )
     user_type = models.CharField(choices=User_type_choices,max_length=50)
-    phone = models.CharField( max_length=20)
-
     def __str__(self):
         return f'{self.username} - {self.user_type}'
     
